6dc65a4582ad99e6a8abdcf461fd808caf7f6440
Will Anderson <will@itsananderson.com>
2015-05-12 12:37:04 -0700

    Add a failing test

--- test/test.ts
+++ test/test.ts
@@ -20,4 +20,8 @@ describe("hot-code-push", function() {
     it("can call an included function", function() {
         assert.equal(test(), "bar123", "Return from test() should be 'bar123'");
     });
+
+    it("can run a failing test", function() {
+        assert.equal(test(), "bar1234", "Return from test() should be 'bar1234'");
+    });
 });
